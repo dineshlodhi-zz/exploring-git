3e4e24cb0e95a0a826e5e3423e6bf503a1f42ba6
dinesh.lodhi <dinesh.lodhi@servicenow.com>
2018-08-19 11:03:39 +0530

    Match and validate Parenthesis in an expression

--- validateParenthesis.cpp
+++ validateParenthesis.cpp
@@ -4,13 +4,13 @@ using namespace std;
 bool isOpeningBracket(char b) {
   if(b == '(' || b == '{' || b == '[')
   	return true;
-  else 
+  else
    return false;
 }
 bool isClosingBracket(char b){
 	if(b == ')' || b == '}' || b == ']')
   		return true;
-  	else 
+  	else
    		return false;
 }
 
@@ -32,13 +32,14 @@ bool validateParenthesis(char s[])
         for(int i=0; i<length; i++){
         	if(isOpeningBracket(s[i])){
              stk.push(s[i]);
-            } else if(!stk.empty() && isClosingBracket(s[i])){                                       	if(isMatchingBracket(stk.top(), s[i])){
+            } else if(!stk.empty() && isClosingBracket(s[i])){
+                 if(isMatchingBracket(stk.top(), s[i])){
                    stk.pop();
                  } else {
-                 	return false ; 
+                 	return false ;
                  }
            }
-                                                             
+
         }
   		// check if stack is empty or not.
   		if(!stk.empty()){
